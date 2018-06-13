14230743f7eafbe4d349e3111ce279f1e0dfc5fc
Adalberto 'Nick' Garza <adagar121987@gmail.com>
2018-06-12 20:01:26 -0400

    Up to vid 29

---
+++ code/CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -0,0 +1,24 @@
+using CSharpFundamentals.Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introduce();
+
+            Calculator calculator = new Calculator();
+            int sum = calculator.Add(1, 2);
+            Console.WriteLine(sum);
+        }
+    }
+}
